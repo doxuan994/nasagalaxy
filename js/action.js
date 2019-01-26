c34fe36670dad38a2364e192b28f1750a0352389
Xuan Do <doxuancntp@gmail.com>
2019-01-25 21:26:43 -0500

    Hook in Vue

--- js/action.js
+++ js/action.js
@@ -1,22 +1,20 @@
-var apiKey = 'WzE03hQYRvEbdAaPUBoc6i4c5QBUfa2eQ0cb4CQr';
-var url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
-
-
 var vm = new Vue({
     el: '#app',
     data: {
         imgSrc: '',
         imgTitle: ''
     },
+    created: function () {
+        this.fetchApod();
+    },
     methods: {
-        process: function() {
-            this.submitted = true;
+        fetchApod: function() {
+            var apiKey = 'WzE03hQYRvEbdAaPUBoc6i4c5QBUfa2eQ0cb4CQr';
+            var url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
+            axios.get(url).then(function (res) {
+                    vm.imgSrc = res.data.url;
+                    vm.imgTitle = res.data.title;
+            });
         }
     }
 })
-
-
-axios.get(url).then(function (res) {
-        vm.imgSrc = res.data.url;
-        vm.imgTitle = res.data.title;
-});
