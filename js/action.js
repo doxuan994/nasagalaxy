8a61396af2f7b17ac1a8c32ff9fe5d3decadbeac
Xuan Do <doxuancntp@gmail.com>
2019-01-25 21:58:46 -0500

    for-each loop in Vue

--- js/action.js
+++ js/action.js
@@ -1,20 +1,24 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        imgSrc: '',
-        imgTitle: ''
+        asteroids: []
     },
     created: function () {
-        this.fetchApod();
+        this.fetchAsteroids();
     },
     methods: {
-        fetchApod: function() {
+        fetchAsteroids: function() {
             var apiKey = 'WzE03hQYRvEbdAaPUBoc6i4c5QBUfa2eQ0cb4CQr';
-            var url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey;
+            var url = 'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=' + apiKey;
             axios.get(url).then(function (res) {
-                    vm.imgSrc = res.data.url;
-                    vm.imgTitle = res.data.title;
+                    vm.asteroids = res.data.near_earth_objects.slice(0, 10);
             });
+        },
+        getCloseApproachDate: function (a) {
+            if (a.close_approach_data.length > 0) {
+                return a.close_approach_data[0].close_approach_date;
+            }
+            return 'N/A';
         }
     }
 })
